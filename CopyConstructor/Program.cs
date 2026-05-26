ac751547ffaf987c46b87efebe6797ef9062e0e2
raghu <raghu@DELL>
2026-05-26 10:55:13 +0530

    Add project files.

---
+++ CopyConstructor/Program.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CopyConstructor
+//{
+//    class student
+//    {
+//        public int Sid;
+//        public string Sname;
+
+//        public student(int sid, string sname)
+//        {
+//            sid = Sid;
+//            sname = Sname;
+//        }
+//        public student(student stu)
+//        {
+//            Sid = stu.Sid;
+//            Sname = stu.Sname;
+//        }
+//        public void StudentDisplay()
+//        {
+//            Console.WriteLine("student id is " + Sid);
+//            Console.WriteLine("student name is " + Sname);
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            student obj = new student(1, "Raghu");
+//            obj.StudentDisplay();
+//            student stu = new student(obj);
+//            stu.StudentDisplay();
+//        }
+//    }
+//}
---
+++ CopyConstructor/Properties/CopyConstructor1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copyConstructor
+{
+    class student
+    {
+        int studentid;
+        string studentname;
+        public student(int sid, string sname)
+        {
+            studentid = sid;
+            studentname = sname;
+        }
+        public student(student sobj)
+        {
+            studentid = sobj.studentid;
+            studentname = sobj.studentname;
+        }
+        public void studentdisplay()
+        {
+            Console.WriteLine("Student id is " + studentid);
+            Console.WriteLine("Student name is " + studentname);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student obj = new student(139, "Raghu");
+            obj.studentdisplay();
+            student stu = new student(obj);
+            stu.studentdisplay();
+        }
+    }
+}
